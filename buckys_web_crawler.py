851abdbb71b27984bd4fb20df05c1abe64d201e4
richardpel <srqty@hotmail.com>
2019-02-01 15:15:54 -0500

    Tuples pour ajouter jour de la semaine. Pas terminer. Ajouter logs.

--- buckys_web_crawler.py
+++ buckys_web_crawler.py
@@ -6,7 +6,7 @@
 import re
 from bs4 import BeautifulSoup
 import urllib.request
-url = 'https://www.aelf.org/calendrier/romain/2019)/02'
+url = 'https://www.aelf.org/calendrier/romain/2019/02'
 req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = urllib.request.urlopen(req).read()
   
@@ -29,32 +29,36 @@ def chercher_date_du_jour(texte_de_messe):
     Retourne: le texte du jour"""
 def chercher_texte_du_jour(texte_de_messe):
     soup_lecture = BeautifulSoup(texte_de_messe, 'html.parser')
-
     texte_complet_liste = [] #créer une liste qui contiendra le texte
-    for texte in soup_lecture.find_all('div', {'id':re.compile('messe1_lecture[0-9]')}):  #Voire Mitchell, faut noter tous les éléments texte_complet_liste.append(texte) #Ajouter les lectures du jour. Voire «List append() in for loop [duplicate]» \ https://stackoverflow.com/questions/41452819/list-append-in-for-loop #Convertir la liste dans un seul string #«Python map() function» https://www.journaldev.com/22960/python-map-function return("".join(map(str, texte_complet_liste))) """Regex pour ajouter le jour de la semaine à la date.""" """Reçoit la date sans jour de la semaine + Retourne: la date avec le jour de la semaine"""
-   def ajouter_jour_de_la_semaine(date_sans_jour):
-        pattern1 = re.compile(r'(<h4 class="date">(1 février 2019)</h4>|<h4 class="date">(8  février 2019)</h4>|<h4 class="date">(15 février 2019)</h4>|<h4 class="date">(22 février 2019)</h4>|<h4 class="date">(28 février 2019)</h4>)') 
-        pattern2 = re.compile(r'(<h4 class="date">(2 février 2019)</h4>|<h4 class="date">(9 février 2019)</h4>|<h4 class="date">(16 février 2019)</h4>|<h4 class="date">(23 février 2019)</h4>|<h4 class="date">(30 février 2019)</h4>)')
-        pattern3 = re.compile(r'(<h4 class="date">(3 février 2019)</h4>|<h4 class="date">(10 février 2019)</h4>|<h4 class="date">(17 février 2019)</h4>|<h4 class="date">(24 février 2019)</h4>|<h4 class="date">(31 février 2019)</h4>)') 
-        pattern4 = re.compile(r'(<h4 class="date">(4 février 2019)</h4>|<h4 class="date">(11 février 2019)</h4>|<h4 class="date">(18 février 2019)</h4>|<h4 class="date">(25 février 2019)</h4>)') 
-        pattern5 = re.compile(r'(<h4 class="date">(5 février 2019)</h4>|<h4 class="date">(12 février 2019)</h4>|<h4 class="date">(19 février 2019)</h4>|<h4 class="date">(26 février 2019)</h4>)')
-        pattern6 = re.compile(r'(<h4 class="date">(6 février 2019)</h4>|<h4 class="date">(13 février 2019)</h4>|<h4 class="date">(20 février 2019)</h4>|<h4 class="date">(27 février 2019)</h4>)')
-        pattern7 = re.compile(r'(<h4 class="date">(7 février 2019)</h4>|<h4 class="date">(14 février 2019)</h4>|<h4 class="date">(21 février 2019)</h4>|<h4 class="date">(28 février 2019)</h4>)')
 
-        if pattern1 in date_sans_jour:  #Python - Basic Operators https://www.tutorialspoint.com/python/python_basic_operators.htm
-            return(pattern1.sub(r'<p>vendredi \1</p>', date_sans_jour)
-        elif pattern2 in date_sans_jour:
-            return(pattern2.sub(r'samedi \1', date_sans_jour))
-        elif pattern3 in date_sans_jour:
-            return(pattern3.sub(r'dimanche \1', date_sans_jour))
+    for texte in soup_lecture.find_all('div', {'id':re.compile('messe1_lecture[0-9]')}):  #Voire Mitchell, faut noter tous les éléments texte_complet_liste.append(texte) 
+        #Ajouter les lectures du jour. Voire «List append() in for loop [duplicate]» \ https://stackoverflow.com/questions/41452819/list-append-in-for-loop 
+        #Convertir la liste dans un seul string 
+        #«Python map() function» https://www.journaldev.com/22960/python-map-function 
+        return("".join(map(str, texte_complet_liste)))
+
+"""Enlever balises html +
+Reçoit: date avec balises html +
+Retourne: string qui contient la date sans balises"""
+def enlever_balises_html(date_du_jour_avec_balises):
+    date_du_jour_sans_balises = date_du_jour_avec_balises.get_text()
         
-        else:
-        assert une erreur voir automate the boring stuff
+"""Ajouter le jour de la semaine à la date.""" 
+"""Reçoit la date sans jour de la semaine + 
+Retourne: la date avec le jour de la semaine"""
+def ajouter_jour_de_la_semaine(date_sans_jour):
+    pattern1 = ('<h4 class="date">1 février 2019</h4>', '<h4 class="date">8  février 2019</h4>', '<h4 class="date">15 février 2019</h4>', '<h4 class="date">22 février 2019</h4>', '<h4 class="date">28 février 2019</h4>)') 
+    pattern2 = ('<h4 class="date">2 février 2019</h4>', '<h4 class="date">9 février 2019</h4>', '<h4 class="date">16 février 2019</h4>', '<h4 class="date">23 février 2019</h4>', '<h4 class="date">30 février 2019</h4>)')
+    pattern3 = ('<h4 class="date">3 février 2019</h4>', '<h4 class="date">10 février 2019</h4>', '<h4 class="date">17 février 2019</h4>', '<h4 class="date">24 février 2019</h4>', '<h4 class="date">31 février 2019</h4>)') 
+    pattern4 = ('<h4 class="date">4 février 2019</h4>', '<h4 class="date">11 février 2019</h4>', '<h4 class="date">18 février 2019</h4>', '<h4 class="date">25 février 2019</h4>)') 
+    pattern5 = ('<h4 class="date">5 février 2019</h4>', '<h4 class="date">12 février 2019</h4>', '<h4 class="date">19 février 2019</h4>', '<h4 class="date">26 février 2019</h4>)')
+    pattern6 = ('<h4 class="date">6 février 2019</h4>', '<h4 class="date">13 février 2019</h4>', '<h4 class="date">20 février 2019</h4>', '<h4 class="date">27 février 2019</h4>)')
+    pattern7 = ('<h4 class="date">7 février 2019</h4>', '<h4 class="date">14 février 2019</h4>', '<h4 class="date">21 février 2019</h4>', '<h4 class="date">28 février 2019</h4>)')
 
-        remplacer4 = pattern4.sub(r' lundi \1', remplacer3)
-        remplacer5 = pattern5.sub(r' mardi \1', remplacer4)
-        remplacer6 = pattern6.sub(r' mercredi \1', remplacer5)
-        remplacer7 = pattern7.sub(r'jeudi \1', lf_contents)
+    if date_sans_jour in pattern1:
+        date_du_jour_sans_balises = enlever_balises_html(date_sans_jour)
+        date_du_jour_<p>_jour = "<p>le vendredi " + date_du_jour_sans_balises + "</p>"
+        return(date_du_jour_<p>_jour)
 
 """Reçoit: texte à ajouter au fichier +
     Retourne: rien"""
@@ -66,9 +70,7 @@ def ajouter_texte_fichier(texte_ajouter):
 if __name__ == "__main__":
     #Python 3.4 urllib.request error (http 403) https://stackoverflow.com/questions/28396036/python-3-4-urllib-request-error-http-403
     """Extraire les liens sur la page du mois"""
-    with open('liens_lectures_du_mois.txt', 'a') as af:
-        
-        
+    with open('liens_lectures_du_mois.txt', 'a') as af:        
 #        source_code = requests.get(url)
 #        source_code.raise_for_status()
 #        plain_text = source_code.text
@@ -93,11 +95,11 @@ with open('liens_lectures_du_mois.txt', 'r') as lf:
         
         """Extraire la date de la page web"""
         date_du_jour = chercher_date_du_jour(lecture_texte)
-       
-       """Ajouter le jour de la semaine à la date"""
-       date_avec_jour = ajouter_jour_de_la semaine(date_du_jour)
-       print(date_avec_jour)
-       ajouter_texte_fichier(date_avec_jour)
+        print('"date_du_jour" est du type: ', type(date_du_jour))      
+        """Ajouter le jour de la semaine à la date"""
+        date_avec_jour = ajouter_jour_de_la_semaine(date_du_jour)
+        
+        ajouter_texte_fichier(date_avec_jour)
 
         """Extraire le texte de la page web"""
         texte_du_jour = chercher_texte_du_jour(lecture_texte)
